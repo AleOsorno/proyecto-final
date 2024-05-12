b805ba206fe828ebc93cac5eb098c7fdc51e32b2
Alejandro <alexosorno991@gmail.com>
2024-05-12 10:56:05 -0600

    Barra lateral normal, hecha.

--- java/script.js
+++ java/script.js
@@ -1,8 +1,11 @@
 const logo = document.getElementById("logo");//guardamos el id del logo en la variable
 const barraLateral = document.querySelector(".contenido_menu");//devuelve el primer contenido del documento o div
-const span = document.querySelector("span");//obtener el primer elemento span
+const spans = document.querySelectorAll("span");//obtener todo elemento span
 
 logo.addEventListener("click",()=>{//Al presionar el icono hara las siguientes acciones
     barraLateral.classList.toggle("mini-barra-lateral");//agrega o quita la clase del css
-    span.classList.toggle("oculto");
+    
+    spans.forEach((span)=>{//De esta manera recorremos todos los span y agregamos o quitamos la clase oculto
+        span.classList.toggle("oculto");
+    });
 });
