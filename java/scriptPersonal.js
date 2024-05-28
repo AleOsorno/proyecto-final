b879a2044a90f71ae2030ba0b77d8ab3806c46b0
Alejandro <alexosorno991@gmail.com>
2024-05-28 15:24:21 -0600

    Pagina del personal terminada

--- java/scriptPersonal.js
+++ java/scriptPersonal.js
@@ -66,3 +66,74 @@ document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mousenter", () => clearTimeout(timeoutId));
 wrapper.addEventListener("mouseleave", autoPlay);
+
+//----------------------------------------------------------------------------------------
+
+const wrapper2 = document.querySelector(".wrapper2");
+const carousel2 = document.querySelector(".carousel2");
+const arrowBtns2 = document.querySelectorAll(".wrapper2 i");
+const firstCardWidth2 = document.querySelector(".card2").offsetWidth;
+const carouselChildrens2 = [...carousel2.children];
+
+let isDragging2 = false, startX2, startScrollLeft2, timeoutId2;
+
+let cardPerView2 = Math.round(carousel2.offsetWidth / firstCardWidth2);
+
+carouselChildrens2.slice(-cardPerView2).reverse().forEach(card2 =>{
+  carousel2.insertAdjacentHTML("afterbegin", card2.outerHTML);
+});
+
+carouselChildrens2.slice(0, cardPerView2).forEach(card2 =>{
+  carousel2.insertAdjacentHTML("beforeend", card2.outerHTML);
+});
+
+arrowBtns2.forEach(btn2 => {
+  btn2.addEventListener("click", () => {
+    carousel2.scrollLeft += btn2.id === "left2" ? -firstCardWidth2 : firstCardWidth2;
+  });
+});
+
+const dragStart2 = (e) =>{
+  isDragging2 = true;
+  carousel2.classList.add("dragging2");
+  startX2 = e.pageX;
+  startScrollLeft = carousel2.scrollLeft;
+}
+
+const dragging2 = (e) =>{
+  if(!isDragging2) return;
+  carousel2.scrollLeft = startScrollLeft - (e.pageX - startX2);
+}
+
+const dragStop2 = () =>{
+  isDragging2 = false;
+  carousel2.classList.remove("dragging2");
+}
+
+const autoPlay2 = () =>{
+  if(window.innerWidth < 800 ) return;
+  timeoutId2 = setTimeout(() => carousel2.scrollLeft += firstCardWidth2, 2500);
+}
+autoPlay2();
+
+const infiniteScroll2 = () =>{
+  if(carousel2.scrollLeft ===0){
+    carousel2.classList.add("no-transition2");
+    carousel2.scrollLeft = carousel2.scrollWidth - (2 * carousel2.offsetWidth);
+    carousel2.classList.remove("no-transition2");
+  }else if(Math.ceil(carousel2.scrollLeft) === carousel2.scrollWidth -carousel2.offsetWidth){
+    carousel2.classList.add("no-transition2");
+    carousel2.scrollLeft = carousel2.offsetWidth;
+    carousel2.classList.remove("no-transition2");
+  }
+
+  clearTimeout(timeoutId2);
+  if(!wrapper2.matches(":hover")) autoPlay2();
+}
+
+carousel2.addEventListener("mousedown", dragStart2);
+carousel2.addEventListener("mousemove", dragging2);
+document.addEventListener("mouseup", dragStop2);
+carousel2.addEventListener("scroll", infiniteScroll2);
+wrapper2.addEventListener("mousenter", () => clearTimeout(timeoutId2));
+wrapper2.addEventListener("mouseleave", autoPlay2);
